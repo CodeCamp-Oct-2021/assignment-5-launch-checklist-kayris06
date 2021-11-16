386d18fe8fed5e0297c92e74769a6f9909c0e814
Krista Valdez <kayris06@gmail.com>
2021-11-16 16:44:08 -0600

    Change Launch Status to Red

--- script.js
+++ script.js
@@ -1,16 +1,59 @@
 // Write your JavaScript code here!
 
-window.addEventListener("load", function() {
+window.addEventListener("load", function () {
 
-   let listedPlanets;
-   // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-   let listedPlanetsResponse;
-   listedPlanetsResponse.then(function (result) {
-       listedPlanets = result;
-       console.log(listedPlanets);
-   }).then(function () {
-       console.log(listedPlanets);
-       // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-   })
-   
+    //    let listedPlanets;
+    //    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
+    //    let listedPlanetsResponse;
+    //    listedPlanetsResponse.then(function (result) {
+    //        listedPlanets = result;
+    //        console.log(listedPlanets);
+    //    }).then(function () {
+    //        console.log(listedPlanets);
+    //        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
+    //    })
+    // on Adding Validation
+
+    //First, we are already inside a window load event listner (line3) Only one single window load event needed in our code
+    //before we worry about validating the form inputs we need to select the form
+    //and listen for the submit event
+    //
+    let form = document.querySelector("form");
+    form.addEventListener("submit", function (event) {
+        //default behavior is that when we submit html forms 
+        //the form sends an http request and refreashes the page
+        //we do not what this to happen. We want the code in 
+        //our form submit even listener to execute. Refreshing the page refreashes the console.
+        
+
+        event.preventDefault();
+        //We need to validate the inputs are correct data types.. number or string
+        // console.log('page hasnt refreashed yet')
+
+        //the rest of the code in this form event listener sould go below this line
+        let pilotName = document.querySelector("input[name=pilotName]");
+        let copilotName = document.querySelector("input[name=copilotName");
+        let fuelLevel = document.querySelector("input[name=fuelLevel");
+        let cargoMass = document.querySelector("input[name=cargoMass");
+        let faultyItems = document.getElementById("faultyItems");
+        // if (pilotName.value === "" || copilotName.value === "" || fuelLevel.value === "" || cargoMass.value === "") {
+        //     alert("All fields are required!"); 
+        // }
+        formSubmission(document,faultyItems, pilotName.value, copilotName.value, fuelLevel.value, cargoMass.value)
+
+        //need to validate our form inputs using the DOM and conditionals
+        // we are going to use the formSubmission function to validate the values of each of our form
+        //inputs, Pilot Name Copilot fuelLever, cargoMass
+        //Before validation of input we need to select all form inputs with the DOM
+        // let input2 = //select an input here 
+        // *****let input 3 = "placeholder for actually selecting the input"
+        // we want to do this 4 times for each element
+        //now that we have all 4 form input elements selected we can 
+        //pass their VALUEs into our formSubmission function
+
+        //very important to understand difference between HTML element and its value!!!
+
+    });
+    //itll return the first form on the page
+
 });
--- scriptHelper.js
+++ scriptHelper.js
@@ -2,33 +2,116 @@
 require('isomorphic-fetch');
 
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
-   // Here is the HTML formatting for our mission target div.
-   /*
-                <h2>Mission Destination</h2>
-                <ol>
-                    <li>Name: </li>
-                    <li>Diameter: </li>
-                    <li>Star: ${star}</li>
-                    <li>Distance from Earth: </li>
-                    <li>Number of Moons: </li>
-                </ol>
-                <img src="">
-   */
+    // Here is the HTML formatting for our mission target div.
+    /*
+                 <h2>Mission Destination</h2>
+                 <ol>
+                     <li>Name: </li>
+                     <li>Diameter: </li>
+                     <li>Star: ${star}</li>
+                     <li>Distance from Earth: </li>
+                     <li>Number of Moons: </li>
+                 </ol>
+                 <img src="">
+    */
 }
 
 function validateInput(testInput) {
-   
-}
 
+}
+//this formSubmission function will be invoked inside 
+//the form submit even Listener in script.js to validate all of our inputs
+// 6 parameters: document, which is just the document object
+// list - the faulty items div
+//piolot - the value of our pilot name input 
+// co pilot - value of the input
+//fuel Level - value of fuelLevel input
+//cargoMass, value of cargoMass input
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
+    if (pilot === "" || copilot === "" || fuelLevel === "" || cargoLevel === "") {
+        alert("All fields are required!");
+    } else if ((isNaN(cargoLevel)) || (isNaN(fuelLevel)) || (!isNaN(pilot)) || (!isNaN(copilot))) {
+        alert("Please enter valid information");
+    } else if (fuelLevel < 10000 || cargoLevel > 10000) {
+        // let list = document.getElementById("list");				
+        list.style.visibility = "visible";			
+        let launchStatus = document.getElementById("launchStatus");
+        launchStatus.style.color = "red";
+        let fuel = document.getElementById(fuelLevel)
+        fuel = "Not enough fuel for the Journey"
+
+
+
+        // <button id="formSubmit">Submit</button>
+    // <button onclick="toggleClock()" id="list">Show clock</button>
+
+
+    // document.getElementById("list")
+    // list.style.visibility = 'visible';
+
+
+    // document.getElementById("list").hidden = false;
+    //     // document.getElementById('list').style.display = 'block';
+    //     // launchStatus.style.color= "red"
+
+    }
+        // element = document.querySelector('list');
+        //    element.style.visibility = 'hidden';
+        // let faultyItems = document.getElementById("list");
+        // faultyItems = 
+        //  list.style.color = "red";
+
    
+        // change faultyItems to visible with an updated fuel status (there is not enough fuel for the Journey.
+        // The text of h2 element, launch status, change to "shuttle not ready for launch and the color should change red.")
+
+
+ 
+
+
+    //VAIDATING INPUTS 
+    //long series of conditionals to validate all information 
+    // given all of the inputs.. determine what are our conditions 
+    /* condition 1: all fields are required!
+          if any field is empty... send alert that reads (all fields required)
+      condition 2: use isNan() to validate input in each field is correct data type
+      if pilot/copilot are numbers (alert user (must be a name))
+      if fuel level/cargo are not numbers (alert user (must be a number ))
+  Updating Status
+  based on conditions if we are at this point and have no alerted the user of bad data
+  we assume we have validated all input properly 
+  we are good to go to check if ready to launch (fuel and cargo)
+  
+  What if fuel level and cargo level are less than 10000
+  what if both are greater than 10000
+  What if one is greater and one is less than 
+  What should we do or display as a restult of these conditions 
+  
+  in any case our list parameter FaultyItems () should be visible 
+  
+  also pilot status and co-pilot status should be updated 
+  to be template literals to read their names 
+  
+  how to update text inside an HTML element 
+  do we have their status html elements selected or need to seltect those to update 
+  
+  if we are ready to launch... fuel is good and mass is low enough 
+  
+  if not we are not ready for launch. Well need to update launchStatus text to reflect whether or not
+   we are ready for launch as well
+  just like pilot and copilot do we need 
+  
+  
+    */
+
+
 }
 
 async function myFetch() {
     let planetsReturned;
 
-    planetsReturned = await fetch().then( function(response) {
-        });
+    planetsReturned = await fetch().then(function (response) {
+    });
 
     return planetsReturned;
 }
@@ -39,5 +122,5 @@ function pickPlanet(planets) {
 module.exports.addDestinationInfo = addDestinationInfo;
 module.exports.validateInput = validateInput;
 module.exports.formSubmission = formSubmission;
-module.exports.pickPlanet = pickPlanet; 
+module.exports.pickPlanet = pickPlanet;
 module.exports.myFetch = myFetch;
