103774574fa4aef3e3e77d255b5b8f450c84bad7
Krista Valdez <kayris06@gmail.com>
2021-11-19 14:38:34 -0600

    changestofinal

--- scriptHelper.js
+++ scriptHelper.js
@@ -16,65 +16,61 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
        <img src="${imageUrl}">`
 }
 
-    function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-        if (pilot === "" || copilot === "" || fuelLevel === "" || cargoLevel === "") {
-            alert("All fields are required!");
-        } else if ((isNaN(cargoLevel)) || (isNaN(fuelLevel)) || (!isNaN(pilot)) || (!isNaN(copilot))) {
-            alert("Please enter valid information");
+function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
+    pilotStatus.textContent = `Pilot ${pilot} is ready for launch`;
+    copilotStatus.textContent = `Co-pilot ${copilot} is ready for launch`;
+    list.style.visibility = "visible";
 
-        } else if (fuelLevel < 10000 && cargoLevel > 10000) {
-            pilotStatus.textContent = `Pilot ${pilot} is ready for launch`
-            copilotStatus.textContent = `Co-pilot ${copilot} is ready for launch`
-            list.style.visibility = "visible";
-            launchStatus.textContent = "Shuttle Not Ready";
-            launchStatus.style.color = "red";
-            fuelStatus.textContent = "There is not enough fuel for the Journey";
-            cargoStatus.textContent = "There is too much cargo"
-        } else if (fuelLevel >= 10000 && cargoLevel < 10000) {
-            pilotStatus.textContent = `Pilot ${pilot} is ready for launch`
-            copilotStatus.textContent = `Co-pilot ${copilot} is ready for launch`
-            list.style.visibility = "visible";
-            launchStatus.textContent = "Shuttle Is Ready For Launch";
-            launchStatus.style.color = "green";
-        } else if (fuelLevel >= 10000 && cargoLevel > 10000) {
-            pilotStatus.textContent = `Pilot ${pilot} is ready for launch`
-            copilotStatus.textContent = `Co-pilot ${copilot} is ready for launch`
-            list.style.visibility = "visible";
-            launchStatus.textContent = "Shuttle Not Ready";
-            launchStatus.style.color = "red";
-            cargoStatus.textContent = "There is too much cargo"
-        } else if (fuelLevel < 10000 && cargoLevel < 10000) {
-            pilotStatus.textContent = `Pilot ${pilot} is ready for launch`
-            copilotStatus.textContent = `Co-pilot ${copilot} is ready for launch`
-            list.style.visibility = "visible";
-            launchStatus.textContent = "Shuttle Not Ready";
-            launchStatus.style.color = "red";
-            fuelStatus.textContent = "There is not enough fuel for the Journey";
-        }
+    if (pilot === "" || copilot === "" || fuelLevel === "" || cargoLevel === "") {
+        alert("All fields are required!");
+    } else if ((isNaN(cargoLevel)) || (isNaN(fuelLevel)) || (!isNaN(pilot)) || (!isNaN(copilot))) {
+        alert("Please enter valid information");
 
-
-    }
-
-    async function myFetch() {
-        let planetsReturned;
-        
-        planetsReturned = await fetch("https://handlers.education.launchcode.org/static/planets.json").then(function (response) {
-            return response.json();
-        });
-
-        return planetsReturned;
-    }
-    
-    function pickPlanet(planets) {
-        
-            let index = Math.floor(Math.random()*planets.length);
-            return planets[index]
+    } else if (fuelLevel < 10000 && cargoLevel > 10000) {
+        launchStatus.textContent = "Shuttle Not Ready";
+        launchStatus.style.color = "red";
+        fuelStatus.textContent = "There is not enough fuel for the Journey";
+        cargoStatus.textContent = "There is too much cargo"
+    } else if (fuelLevel >= 10000 && cargoLevel < 10000) {
+        launchStatus.textContent = "Shuttle Is Ready For Launch";
+        launchStatus.style.color = "green";
+        fuelStatus.textContent = "Fuel level high enough for launch"
+        cargoStatus.textContent ="Cargo mass low enough for launch"
+    } else if (fuelLevel >= 10000 && cargoLevel > 10000) {
+        launchStatus.textContent = "Shuttle Not Ready";
+        launchStatus.style.color = "red";
+        fuelStatus.textContent = "Fuel level high enough for launch"
+        cargoStatus.textContent = "There is too much cargo"
+    } else if (fuelLevel < 10000 && cargoLevel < 10000) {
+        launchStatus.textContent = "Shuttle Not Ready";
+        launchStatus.style.color = "red";
+        fuelStatus.textContent = "There is not enough fuel for the Journey";
+        cargoStatus.textContent ="Cargo mass low enough for launch"
     }
 
 
+}
 
-    module.exports.addDestinationInfo = addDestinationInfo;
-    module.exports.validateInput = validateInput;
-    module.exports.formSubmission = formSubmission;
-    module.exports.pickPlanet = pickPlanet;
-    module.exports.myFetch = myFetch;
+async function myFetch() {
+    let planetsReturned;
+
+    planetsReturned = await fetch("https://handlers.education.launchcode.org/static/planets.json").then(function (response) {
+        return response.json();
+    });
+
+    return planetsReturned;
+}
+
+function pickPlanet(planets) {
+
+    let index = Math.floor(Math.random() * planets.length);
+    return planets[index]
+}
+
+
+
+module.exports.addDestinationInfo = addDestinationInfo;
+module.exports.validateInput = validateInput;
+module.exports.formSubmission = formSubmission;
+module.exports.pickPlanet = pickPlanet;
+module.exports.myFetch = myFetch;
